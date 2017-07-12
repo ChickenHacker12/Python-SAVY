90111e6f1aef2ef1fad80524a312fde31137d185
Team445craft <treyeowen@gmail.com>
2017-07-12 09:55:06 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -1,5 +1,7 @@
-# Name:
-# Date:
+import random
+
+# Name: Trey & Laila
+# Date: July 2017
 
 
 """ 
@@ -11,5 +13,62 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+
+counter = 0
+points = 0
+
+print ""
+print "Welcome to the Guessing Game!"
+enter = raw_input("Press ENTER")
+print ""
+number = random.randint(1,9)
+#guess = raw_input("Guess my number, 1 to 9(enter 0 to end game):")
+number = int(number)
+#True = guess != number
+loop = True
+#if guess == number:
+   # print "Congratulations, you guessed my number!"
+
+
+
+
+while loop == True:
+    guess = raw_input("Guess my number, 1 to 9 (enter 0 to end game): ")
+    guess = int(guess)
+    if guess > number:
+        print ""
+        print "Your guess is too high."
+        counter = counter + 1
+        points = points - 1
+
+    elif guess < number:
+        if guess == 0:
+            loop = False
+            print "\nYou exited the game."
+            print "Your total points: ", points
+        elif guess < number:
+            print ""
+            print "Your guess is too low."
+            counter = counter + 1
+            points = points - 1
+
+    elif guess == number:
+        points = points + 5
+        print "\nYou guessed my number! Congratulations!"
+        print "You total points: ", points
+        print ""
+        number = random.randint(1,9)
+
+        #loop = False
+
+
+
+
+
+
+
+
+
+
 
 
